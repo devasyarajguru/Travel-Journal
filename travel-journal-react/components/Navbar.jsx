5d0d0ab18d8ccec348ab76a1e543e78b426fa410
Devasya <devasyarajguru@gmail.com>
2023-10-23 16:58:27 +0530

    travel-journal-react

---
+++ travel-journal-react/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import PublicIcon from '@mui/icons-material/Public';
+export default function Navbar()
+{
+ return(
+    <>
+        <nav className="navbar--container">
+        <PublicIcon/><span>My Travel Journal</span> 
+        </nav>
+    </>
+ )       
+}
